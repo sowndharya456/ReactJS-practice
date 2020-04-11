b7b5516954abc2a256f4e281a57882ce967ed6c3
sowndharya456 <sowndharyar12@gmail.com>
2020-04-11 13:56:15 +0530

    Displayed Phone Directory

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function App() {
-   return( <div>
-
-    Phone Directory
-  </div>
-   );
+   render() {
+      return (
+        <div>
+          Phone Directory
+          <div className="header">
+            Phone Directory
+          </div>
+          <button>Add</button>
+          <div>
+            <span>Name</span><br />
+            <span>Phone</span>
+          </div>
+        </div>
+      );    
 }
 
 export default App;
