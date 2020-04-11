fecbfe46b8fbe11bdad7ee2d387714ab146ca71b
sowndharya456 <sowndharyar12@gmail.com>
2020-04-11 13:50:08 +0530

    Displayed Phone Directory

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function App() {
-    return ( < div > < /div>);
-    }
+   return( <div>
 
-    export default App;
+    Phone Directory
+  </div>
+   );
+}
+
+export default App;
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//     // TODO 1: Remove heading tag with text "Hello World"
+//     // TODO 2: Render a textbox (in place of heading tag) with type text and placeholder "Your Name"
+//     <div >
+//     <input type = "text" placeholder = "Enter text" />
+//     </div>
+// );
