acce9e111b5b946e8946356cadcc5cb074b2d18a
sowndharya456 <sowndharyar12@gmail.com>
2020-04-15 15:06:01 +0530

    Styled ‘Header’ (External Styling)

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="header">
-          Phone Directory
-        </div>
         <Header/>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+        <div className="component-body-container">
+        <button className="addBtn" >Add</button>
+        <div className="grid-container">
+          <span >Name</span><br />
+          <span >Phone</span>
+        </div>
         </div>
       </div>
     );
--- phone-directory/src/Header.js
+++ phone-directory/src/Header.js
@@ -1,19 +1,13 @@
 import React from 'react';
+import './Header.css';
 
 const Header = function () {
-    const headerStyle = {
-        textAlign: 'center',
-        padding: 20,
-        background: '#000',
-        color: '#fff',
-        textTransform: 'uppercase'
-    };
 
     return (
       
-        <div style={headerStyle}>
-            Phone Directory
-        </div>
-    )
+        <div className="header">
+        Phone Directory
+    </div>
+)
 }
 export default Header;
