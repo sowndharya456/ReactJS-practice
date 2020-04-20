50b16e1820573d34e5ae56851d69192044e51574
sowndharya456 <sowndharyar12@gmail.com>
2020-04-20 13:01:15 +0530

    Added ‘PhoneDirectory as smart component, renamed ‘App’ as ‘ShowSubscribers’ and converted ‘AddSubscribers’ and ‘ShowSubscribers’ as dumb components

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react';
-import Header from './Header.js';
-import './App.css';
+// import React, { Component } from 'react';
+// import Header from './Header.js';
+// import './App.css';
 
-class App extends Component {
+// class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
-    }
-  }
-  render() {
+//   constructor() {
+//     super();
+//     this.state = {
+//       subscribersListToShow: []
+//     }
+//   }
+//   render() {
 
-    return (
-      <div>
-        <Header heading="Phone Directory" />
-        <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
-          <div className="grid-container heading-container">
-            <span className="grid-item name-heading">Name</span>
-            <span className="grid-item phone-heading">Phone</span>
-          </div>
+//     return (
+//       <div>
+//         <Header heading="Phone Directory" />
+//         <div className="component-body-container">
+//           <button className="custom-btn add-btn">Add</button>
+//           <div className="grid-container heading-container">
+//             <span className="grid-item name-heading">Name</span>
+//             <span className="grid-item phone-heading">Phone</span>
+//           </div>
 
-          {
-               this.state.subscribersListToShow.map(sub => {
-              return <div key={sub.id} className="grid-container">
-                <span className="grid-item">{sub.name}</span>
-                <span className="grid-item">{sub.phone}</span>
-                <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
-                </span>
-              </div>
-            })
-          }
-        </div>
-      </div>
-    );
-  }
-}
-export default App;
+//           {
+//                this.state.subscribersListToShow.map(sub => {
+//               return <div key={sub.id} className="grid-container">
+//                 <span className="grid-item">{sub.name}</span>
+//                 <span className="grid-item">{sub.phone}</span>
+//                 <span className="grid-item action-btn-container">
+//                   <button className="custom-btn delete-btn">Delete</button>
+//                 </span>
+//               </div>
+//             })
+//           }
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+ //***************Renamed to showSubscriber************************ */
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
